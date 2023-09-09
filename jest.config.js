1026384de0bd0c0592b1a723e83b7e45aac781ad
Sebastian Preston <38880204+sebprest@users.noreply.github.com>
2023-09-09 15:36:42 +0700

    chore: configure path mapping

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,10 @@ module.exports = createJestConfig({
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['./jest.setup.ts'],
+  moduleNameMapper: {
+    '^components/(.*)$': '<rootDir>/src/components/$1',
+    '^pages/(.*)$': '<rootDir>/src/pages/$1',
+    '^queries/(.*)$': '<rootDir>/src/queries/$1',
+    '^types/(.*)$': '<rootDir>/src/types/$1'
+  },
 });
